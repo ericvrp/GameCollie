b17ece6e1a02b52dd1e08755d86d45a07c354c06
Eric van Riet Paap <ericvrp@gmail.com>
2017-04-23 19:58:35 +0200

    resolve platform names to what RetroPie uses. show what gamelist.xml files are found

--- GameCollie.js
+++ GameCollie.js
@@ -1,19 +1,49 @@
 const settings   = require('./settings.json')
-console.log(JSON.stringify(settings, null, 2))
+// console.log(JSON.stringify(settings, null, 2))
 
 const fs         = require('fs')
 const xmldoc     = require('xmldoc') // https://github.com/nfarina/xmldoc
 const thegamesdb = require('thegamesdb') // https://github.com/nauzethc/thegamesdb-api && http://wiki.thegamesdb.net/index.php/API_Introduction
 
+// returns list of subdirectories
+const lsdir = p => fs.readdirSync(p).filter(f => fs.statSync(p+'/'+f).isDirectory())
 
-const filename = process.argv[2] || 'testgamelists/psx/gamelist.xml'
-const xml = fs.readFileSync(filename, 'utf8')
-const gameList = new xmldoc.XmlDocument(xml)
-const games = gameList.childrenNamed('game')
-for (const game of games) {
-  if (!game.attr.id) continue
-  thegamesdb.getGame({id: game.attr.id}).then(result => {
-    console.log(filename, 'is for', result.platform)
-  })
-  break
+const logPlatform = (gamelistXml) => {
+  // console.log(gamelistXml) // const gamelistXml = settings.gameCollection.src + '/psx/gamelist.xml'
+
+  const xml = fs.readFileSync(gamelistXml, 'utf8')
+  const gameList = new xmldoc.XmlDocument(xml)
+  const games = gameList.childrenNamed('game')
+
+  for (const game of games) {
+    if (!game.attr.id) continue
+    // note: we could cash the platform of all games so we don't have to access thegamedb
+    // console.log(game.attr)
+    thegamesdb.getGame({id: game.attr.id}).then(result => {
+      console.log(gamelistXml, '=', result.platform)
+    }).catch(reason => console.error(reason))
+    break
+  }
+}
+
+const getDstPlatform = (srcPlatform) => {
+  for (const dstPlatform in settings.platformAliases) {
+    const s = srcPlatform.toLowerCase()
+    if (s === dstPlatform || settings.platformAliases[dstPlatform].includes(s)) {
+      return dstPlatform
+    }
+  }
+  return '<unknown platform>'
+}
+
+// main
+const srcPlatforms = lsdir(settings.gameCollection.src)
+for (const srcPlatform of srcPlatforms) {
+  const gamelistXml = settings.gameCollection.src + '/' + srcPlatform + '/gamelist.xml'
+  const hasGameListXml = fs.existsSync(gamelistXml)
+
+  const dstPlatform = getDstPlatform(srcPlatform)
+  console.log(srcPlatform, '=>', dstPlatform, hasGameListXml ? '' : '(no gamelist.xml)')
+
+  // logPlatform(gamelistXml)
 }
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ with npm:
 	npm install	# to install
 	npm start	# to run
 
+notes:
+	- the scraper from https://github.com/sselph/scraper/releases generates gamelist.xml files in the desired format.
+	- per platform folder run: scraper -thumb_only -append -add_not_found -workers 10
