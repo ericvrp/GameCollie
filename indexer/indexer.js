b1b47a0d7c85d99b0f83de05e5d6f6c91179144c
Eric van Riet Paap <ericvrp@gmail.com>
2017-05-03 21:48:15 +0200

    meteor update

--- indexer/indexer.js
+++ indexer/indexer.js
@@ -30,7 +30,45 @@ const isSkippedFile = (filename) => {
   return false
 }
 
-var walk = function(directoryName) {
+const hashFiles = (filelist, algo) => {
+
+  // const _hashFile = (filename) => {
+  //
+  //     console.log(filename)
+  //     const crc32_ = crc32.unsigned(fs.readFileSync(filename)).toString(16)
+  //     Promise.join(hashFile(filename, 'md5'), hashFile(filename, 'sha256'), (md5_, sha256_) => {
+  //       console.log(`  \{"path":"${filename}", "sha256":"0x${sha256_.toUpperCase()}", "md5":"0x${md5_.toUpperCase()}", "crc32":"0x${crc32_.toUpperCase()}"\},`)
+  //     })
+  //
+  // }
+
+  var queued = [], parallel = 3;
+  var hashPromises = filelist.map(function(filename) {
+    // How many items must download before fetching the next?
+    // The queued, minus those running in parallel, plus one of
+    // the parallel slots.
+    var mustComplete = Math.max(0, queued.length - parallel + 1);
+    console.log('queued.length', queued.length, 'mustComplete', mustComplete, 'parallel', parallel, 'filename', filename)
+    // when enough items are complete, queue another request for an item
+    return Promise.some(queued, mustComplete)
+      .then(function() {
+          // var download = _hashFile(id);
+          var hf = hashFile(filename, algo)
+          queued.push(hf);
+          return hf;
+      }).then(function(hash) {
+          console.log(hash)
+          // after that new download completes, get the hash.
+          return hash;
+      });
+  });
+
+  Promise.all(hashPromises).then(function(hashes) {
+      console.log('All', algo, 'done. names.length', hashes.length)
+  });
+}
+
+var walk = function(directoryName, filelist=[]) {
   const files = fs.readdirSync(directoryName)
 
   files.forEach(function(file) {
@@ -40,15 +78,17 @@ var walk = function(directoryName) {
     const f = fs.statSync(d)
 
     if (f.isDirectory()) {
-      walk(d)
-    } else {
-      // console.log(d)
-      const crc32_ = crc32.unsigned(fs.readFileSync(d)).toString(16)
-      Promise.join(hashFile(d, 'md5'), hashFile(d, 'sha256'), (md5_, sha256_) => {
-        console.log(`  \{"path":"${d}", "sha256":"0x${sha256_.toUpperCase()}", "md5":"0x${md5_.toUpperCase()}", "crc32":"0x${crc32_.toUpperCase()}"\},`)
-      })
+      walk(d, filelist)
+    }
+    else {
+      filelist.push(d)
     }
   })
+
+  return filelist
 }
 
-walk('.')
+const filelist = walk('.')
+console.log('filelist.length', filelist.length)
+hashFiles(filelist, 'md5')
+// hashFiles(filelist, 'sha256')
