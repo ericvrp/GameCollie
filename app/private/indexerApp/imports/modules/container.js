42668caeefb4adf1bd1b47a742dc84e1b9e6e1ce
Eric van Riet Paap <ericvrp@gmail.com>
2017-05-06 22:25:14 +0200

    WIP desktop app

--- app/private/indexerApp/imports/modules/container.js
+++
@@ -1,6 +0,0 @@
-import { compose } from 'react-komposer';
-import getTrackerLoader from './get-tracker-loader';
-
-export default function container(composer, Component, options = {}) {
-  return compose(getTrackerLoader(composer), options)(Component);
-}
